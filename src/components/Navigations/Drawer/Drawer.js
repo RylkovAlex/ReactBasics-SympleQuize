89e7c0a3772bce5ac3f391358b7da12dac4d2c8f
RylkovAlex <32511612+RylkovAlex@users.noreply.github.com>
2019-10-05 16:52:18 +0300

    добавляет затемнение и закрытие меню по клику

--- src/components/Navigations/Drawer/Drawer.js
+++ src/components/Navigations/Drawer/Drawer.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import classes from './Drawer.module.css'
+import Backdrop from '../../UI/Backdrop/Backdrop'
 
 const links = [1,2,3];
 
@@ -9,7 +10,7 @@ class Drawer extends Component {
       links.map((link, index) => {
         return (
           <li key = {index}>
-            <a href='#'>Link {link}</a>
+            <a>Link {link}</a>
           </li>
         )
       })
@@ -21,11 +22,14 @@ class Drawer extends Component {
       cls.push(classes.close)
     }
     return (
-      <nav className = {cls.join(' ')}>
-        <ul>
-          {this.renderLinks()}
-        </ul>
-      </nav>
+      <React.Fragment>
+        <nav className = {cls.join(' ')}>
+          <ul>
+            {this.renderLinks()}
+          </ul>
+        </nav>
+        {this.props.isOpen ? <Backdrop onClick = {this.props.onClose}/> : null}
+      </React.Fragment>
     )
   }
 }
