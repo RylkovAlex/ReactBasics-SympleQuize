80cdb63075f6a8e390ed99bedfe7b2494514344e
RylkovAlex <32511612+RylkovAlex@users.noreply.github.com>
2019-10-05 16:42:30 +0300

    добавляет менюшку

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
 import classes from './Layout.module.css';
+import MenuToggle from '../../components/Navigations/MenuToggle/MenuToggle'
+import Drawer from '../../components/Navigations/Drawer/Drawer'
 
 class Layout extends Component {
+  state = {
+    menu: false
+  }
+  toggleMenuHandler = () => {
+    this.setState ({
+      menu: !this.state.menu
+    })
+  }
 
   render() {
     return (
     <div className = {classes.Layout}>
+      <Drawer
+        isOpen = {this.state.menu}
+      />
+
+      <MenuToggle
+        onToggle = {this.toggleMenuHandler}
+        isOpen = {this.state.menu}
+      >
+      </MenuToggle>
       <main>
         {this.props.children}
       </main>
